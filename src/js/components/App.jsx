7c102e47df382d39e0bb97679a6958bdc521af2d
Jim <vic20087cjimlin@gmail.com>
2017-02-27 00:18:09 +0800

    add firebase

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -4,10 +4,19 @@ import Radium, { StyleRoot } from 'radium'
 
 @Radium
 export default class extends Component {
+    constructor (props) {
+        super(props)
+        this.show = this.show.bind(this)
+    }
+    show () {
+        console.log(this.props)
+    }
     render () {
         return (
             <StyleRoot>
-                <div>XD</div>
+                <div onClick={this.show}>XD</div>
+                <ul>
+                </ul>
                 { process.env.NODE_ENV !== 'production' ? <Containers.DevTools/> : null }
             </StyleRoot>
         )
